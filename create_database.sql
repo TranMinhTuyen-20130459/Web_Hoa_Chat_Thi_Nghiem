b60a3571128ba78389fb494a8dc338c4c0aeafe2
Trần Minh Tuyên <114179423+TranMinhTuyen-20130459@users.noreply.github.com>
2023-01-07 17:19:34 +0700

    tạo bảng cho project

---
+++ create_database.sql
@@ -0,0 +1,319 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 100424
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : hoa_chat_thi_nghiem
+
+ Target Server Type    : MySQL
+ Target Server Version : 100424
+ File Encoding         : 65001
+
+ Date: 04/01/2023 15:54:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for account_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `account_admin`;
+CREATE TABLE `account_admin`  (
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `id_role_admin` int NOT NULL,
+  `id_status_acc` int NOT NULL,
+  `passwordAD` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `time_created` timestamp NOT NULL DEFAULT current_timestamp,
+  `time_change_pass` timestamp NOT NULL DEFAULT current_timestamp,
+  `fullname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`username`) USING BTREE,
+  INDEX `id_status_acc`(`id_status_acc` ASC) USING BTREE,
+  INDEX `id_role_admin`(`id_role_admin` ASC) USING BTREE,
+  CONSTRAINT `account_admin_ibfk_1` FOREIGN KEY (`id_status_acc`) REFERENCES `status_acc` (`id_status_acc`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `account_admin_ibfk_2` FOREIGN KEY (`id_role_admin`) REFERENCES `role_admin` (`id_role_admin`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for account_customer
+-- ----------------------------
+DROP TABLE IF EXISTS `account_customer`;
+CREATE TABLE `account_customer`  (
+  `id_user_customer` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pass` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `id_status_acc` int NOT NULL,
+  `id_city` int NOT NULL,
+  `fullname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `sex` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `email_customer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `phone_customer` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `time_created` timestamp NOT NULL DEFAULT current_timestamp,
+  `time_change_pass` timestamp NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (`id_user_customer`) USING BTREE,
+  UNIQUE INDEX `username`(`username` ASC) USING BTREE,
+  INDEX `id_status_acc`(`id_status_acc` ASC) USING BTREE,
+  INDEX `id_city`(`id_city` ASC) USING BTREE,
+  CONSTRAINT `account_customer_ibfk_1` FOREIGN KEY (`id_status_acc`) REFERENCES `status_acc` (`id_status_acc`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `account_customer_ibfk_2` FOREIGN KEY (`id_city`) REFERENCES `city` (`id_city`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for bill_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `bill_detail`;
+CREATE TABLE `bill_detail`  (
+  `id_bill` int NOT NULL,
+  `id_product` int NOT NULL,
+  `quantity` int NOT NULL,
+  `listed_price` bigint NOT NULL,
+  `current_price` bigint NOT NULL,
+  PRIMARY KEY (`id_bill`, `id_product`) USING BTREE,
+  INDEX `id_product`(`id_product` ASC) USING BTREE,
+  CONSTRAINT `bill_detail_ibfk_1` FOREIGN KEY (`id_bill`) REFERENCES `bills` (`id_bill`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `bill_detail_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for bills
+-- ----------------------------
+DROP TABLE IF EXISTS `bills`;
+CREATE TABLE `bills`  (
+  `id_bill` int NOT NULL AUTO_INCREMENT,
+  `id_user` int NOT NULL,
+  `id_status_bill` int NOT NULL,
+  `id_city` int NOT NULL,
+  `fullname_customer` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_customer` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_customer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address_customer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bill_price` double NOT NULL,
+  `total_price` double NOT NULL,
+  `time_order` timestamp NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (`id_bill`) USING BTREE,
+  INDEX `id_user`(`id_user` ASC) USING BTREE,
+  INDEX `id_status_bill`(`id_status_bill` ASC) USING BTREE,
+  INDEX `id_city`(`id_city` ASC) USING BTREE,
+  CONSTRAINT `bills_ibfk_2` FOREIGN KEY (`id_status_bill`) REFERENCES `status_bill` (`id_status_bill`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `bills_ibfk_3` FOREIGN KEY (`id_city`) REFERENCES `city` (`id_city`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `bills_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `account_customer` (`id_user_customer`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for city
+-- ----------------------------
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city`  (
+  `id_city` int NOT NULL AUTO_INCREMENT,
+  `name_city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `transport` bigint NOT NULL,
+  PRIMARY KEY (`id_city`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for comment_news
+-- ----------------------------
+DROP TABLE IF EXISTS `comment_news`;
+CREATE TABLE `comment_news`  (
+  `id_comment` int NOT NULL AUTO_INCREMENT,
+  `id_news` int NOT NULL,
+  `id_user_customer` int NOT NULL,
+  `time_comment` timestamp NOT NULL DEFAULT current_timestamp,
+  `content_comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_comment`) USING BTREE,
+  INDEX `id_news`(`id_news` ASC) USING BTREE,
+  INDEX `id_user_customer`(`id_user_customer` ASC) USING BTREE,
+  CONSTRAINT `comment_news_ibfk_1` FOREIGN KEY (`id_news`) REFERENCES `news` (`id_news`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `comment_news_ibfk_2` FOREIGN KEY (`id_user_customer`) REFERENCES `account_customer` (`id_user_customer`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for contact
+-- ----------------------------
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE `contact`  (
+  `id_contact` int NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_contact` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_contact` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `problem_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content_problem` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time_insert` timestamp NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (`id_contact`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for infor_web
+-- ----------------------------
+DROP TABLE IF EXISTS `infor_web`;
+CREATE TABLE `infor_web`  (
+  `id_infor` int NOT NULL AUTO_INCREMENT,
+  `phone_web` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_web` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address_web` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_infor`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for news
+-- ----------------------------
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news`  (
+  `id_news` int NOT NULL AUTO_INCREMENT,
+  `title_news` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content_news` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `url_img_news` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `date_posted` timestamp NOT NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+  `quantity_comment` int NOT NULL,
+  `nameAdmin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_news`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for price_product
+-- ----------------------------
+DROP TABLE IF EXISTS `price_product`;
+CREATE TABLE `price_product`  (
+  `id_product` int NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp,
+  `listed_price` bigint NOT NULL,
+  `current_price` bigint NOT NULL,
+  `nameAdmin` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_product`, `date`) USING BTREE,
+  CONSTRAINT `price_product_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products`  (
+  `id_product` int NOT NULL AUTO_INCREMENT,
+  `id_subtype` int NOT NULL,
+  `id_status_product` int NOT NULL,
+  `id_supplier` int NOT NULL,
+  `name_product` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description_product` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `url_img_product` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `quantity_product` int NOT NULL,
+  `date_inserted` timestamp NOT NULL DEFAULT current_timestamp,
+  `star_review` tinyint NULL DEFAULT NULL,
+  `nameAdmin` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `views` int NULL DEFAULT 0,
+  PRIMARY KEY (`id_product`) USING BTREE,
+  INDEX `id_type_product`(`id_subtype` ASC) USING BTREE,
+  INDEX `id_status_product`(`id_status_product` ASC) USING BTREE,
+  INDEX `id_supplier`(`id_supplier` ASC) USING BTREE,
+  CONSTRAINT `fk_id_subtype_4` FOREIGN KEY (`id_subtype`) REFERENCES `subtype_product` (`id_subtype`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_subtype`) REFERENCES `subtype_product` (`id_subtype`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`id_status_product`) REFERENCES `status_product` (`id_status_product`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `products_ibfk_3` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for review_product
+-- ----------------------------
+DROP TABLE IF EXISTS `review_product`;
+CREATE TABLE `review_product`  (
+  `id_review` int NOT NULL AUTO_INCREMENT,
+  `id_product` int NOT NULL,
+  `stars` tinyint NULL DEFAULT 0,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `fullname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `phone` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_review`, `id_product`) USING BTREE,
+  INDEX `fk_review_product`(`id_product` ASC) USING BTREE,
+  CONSTRAINT `fk_review_product` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for role_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `role_admin`;
+CREATE TABLE `role_admin`  (
+  `id_role_admin` int NOT NULL,
+  `name_role` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_role_admin`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sold_product
+-- ----------------------------
+DROP TABLE IF EXISTS `sold_product`;
+CREATE TABLE `sold_product`  (
+  `id_product` int NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT current_timestamp,
+  `quantity_sold` int NOT NULL,
+  PRIMARY KEY (`id_product`, `datetime`) USING BTREE,
+  CONSTRAINT `sold_product_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for status_acc
+-- ----------------------------
+DROP TABLE IF EXISTS `status_acc`;
+CREATE TABLE `status_acc`  (
+  `id_status_acc` int NOT NULL,
+  `name_status_acc` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_status_acc`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for status_bill
+-- ----------------------------
+DROP TABLE IF EXISTS `status_bill`;
+CREATE TABLE `status_bill`  (
+  `id_status_bill` int NOT NULL,
+  `name_status_bill` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_status_bill`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for status_product
+-- ----------------------------
+DROP TABLE IF EXISTS `status_product`;
+CREATE TABLE `status_product`  (
+  `id_status_product` int NOT NULL,
+  `name_status_product` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_status_product`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for subtype_product
+-- ----------------------------
+DROP TABLE IF EXISTS `subtype_product`;
+CREATE TABLE `subtype_product`  (
+  `id_subtype` int NOT NULL,
+  `name_subtype` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id_type_product` int NOT NULL,
+  `sign` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_subtype`) USING BTREE,
+  INDEX `id_type_product`(`id_type_product` ASC) USING BTREE,
+  CONSTRAINT `subtype_product_ibfk_1` FOREIGN KEY (`id_type_product`) REFERENCES `type_product` (`id_type_product`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for suppliers
+-- ----------------------------
+DROP TABLE IF EXISTS `suppliers`;
+CREATE TABLE `suppliers`  (
+  `id_supplier` int NOT NULL AUTO_INCREMENT,
+  `name_supplier` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_supplier`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for type_product
+-- ----------------------------
+DROP TABLE IF EXISTS `type_product`;
+CREATE TABLE `type_product`  (
+  `id_type_product` int NOT NULL AUTO_INCREMENT,
+  `name_type_product` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_type_product`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
