319d5a2bdca6a63752ae94ab3db0c6e586b6e7db
Trần Minh Tuyên <114179423+TranMinhTuyen-20130459@users.noreply.github.com>
2023-03-22 23:55:09 +0700

    Update alter_table_pro_ver_1.sql

--- alter_table_pro_ver_1.sql
+++ alter_table_pro_ver_1.sql
@@ -5,3 +5,7 @@ ALTER TABLE account_customers
 -- cột id_user_gg để lưu thông tin về tài khoản Gg người dùng đăng nhập vào hệ thống , nếu người dùng không đăng nhập = tài khoản Gg thì cột này có giá trị null     
 ALTER TABLE account_customers
 		ADD COLUMN id_user_gg VARCHAR(50) UNIQUE;
+		
+-- đổi giá trị của cột user_id trong bảng log từ bigint sang varchar(50) 		
+ALTER TABLE `logs`
+	MODIFY COLUMN user_id VARCHAR(50);
