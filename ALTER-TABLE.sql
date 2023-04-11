97eb2cd4e7c2adef437602b5e9d5ad66efb079be
Trần Minh Tuyên <114179423+TranMinhTuyen-20130459@users.noreply.github.com>
2023-04-11 12:48:35 +0700

    Create ALTER-TABLE.sql

---
+++ ALTER-TABLE.sql
@@ -0,0 +1,8 @@
+ALTER TABLE bills ALTER COLUMN shop_id SET DEFAULT "1";
+ALTER TABLE bills ALTER COLUMN from_name SET DEFAULT "k46-it-nlu";
+ALTER TABLE bills ALTER COLUMN from_phone SET DEFAULT "123456789";
+ALTER TABLE bills ALTER COLUMN from_address SET DEFAULT "Khoa CNTT - Đại học Nông Lâm";
+ALTER TABLE bills ALTER COLUMN from_ward_name SET DEFAULT "Linh Trung";
+ALTER TABLE bills ALTER COLUMN from_district_name SET DEFAULT "Thủ Đức";
+ALTER TABLE bills ALTER COLUMN from_province_name SET DEFAULT "Hồ Chí Minh";
+
