62b3e6b471d11f9fedab3e5aea8a4c0aa2e9f4e7
tuyen <20130459@st.hcmuaf.edu.vn>
2023-04-20 20:10:41 +0700

    thêm file facebook.properties và class FacebookProperties

---
+++ HoaChatThiNghiem/src/main/java/properties/FacebookProperties.java
@@ -0,0 +1,38 @@
+package properties;
+
+import database.DbProperties;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class FacebookProperties {
+
+    private static final String PROP_PATH = "facebook.properties";
+    private static final Properties PROP = new Properties();
+
+    static {
+        try {
+            PROP.load(DbProperties.class.getClassLoader().getResourceAsStream(PROP_PATH));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String FACEBOOK_CLIENT_ID() {
+        return PROP.getProperty("clientID");
+    }
+
+    public static String FACEBOOK_CLIENT_SECRET() {
+        return PROP.getProperty("clientSecret");
+    }
+
+    public static String FACEBOOK_REDIRECT_URL() {
+        return PROP.getProperty("redirectUrl");
+    }
+
+    public static String FACEBOOK_LINK_GET_TOKEN(){
+        return PROP.getProperty("linkGetToken");
+    }
+
+
+}
