7b65dcbcd7382078565968156455203c6fc5f896
tuyen <20130459@st.hcmuaf.edu.vn>
2023-05-09 21:55:31 +0700

    nâng cấp chức năng ghi Log khi cập nhật trạng thái đơn hàng

--- HoaChatThiNghiem/src/main/java/controller/admin/bill/AjaxBillUpdateServlet.java
+++ HoaChatThiNghiem/src/main/java/controller/admin/bill/AjaxBillUpdateServlet.java
@@ -1,7 +1,12 @@
 package controller.admin.bill;
 
+import bean.Log;
+import database.JDBiConnector;
+import model.admin.Admin;
 import model.shop.Bill_MT;
 import service.BillService_MT;
+import utils.CommonString;
+import utils.WritingLogUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -20,12 +25,37 @@ public class AjaxBillUpdateServlet extends HttpServlet {
 
         String IdBill = request.getParameter("IdBill");
         String IdStatusBill = request.getParameter("IdStatusBill");
+        Admin admin = (Admin) request.getSession().getAttribute(CommonString.ADMIN_SESSION);
 
         try {
+
+            String contentLog = "";
+            switch (Integer.parseInt(IdStatusBill)) {
+                case 1:
+                    contentLog = "Cập nhật trạng thái đơn hàng về 'CHỜ XÁC NHẬN'";
+                    break;
+                case 2:
+                    contentLog = "Cập nhật trạng thái đơn hàng về 'ĐANG GIAO HÀNG'";
+                    break;
+                case 3:
+                    contentLog = "Cập nhật trạng thái đơn hàng về 'ĐÃ GIAO'";
+                    break;
+                case 4:
+                    contentLog = "Cập nhật trạng thái đơn hàng về 'HỦY ĐƠN HÀNG'";
+                    break;
+                default:
+                    contentLog = "";
+            }
+
             Bill_MT bill = new Bill_MT(Integer.parseInt(IdBill), Integer.parseInt(IdStatusBill));
             boolean checkUpdateBill = BillService_MT.updateInforBill(bill);
-            if (checkUpdateBill) response.getWriter().write("true");
-            else
+            if (checkUpdateBill) {
+                response.getWriter().write("true");
+
+                Log logUpdateBill = new Log(3, "-1", admin.getUsername(), contentLog, "Thành công");
+                WritingLogUtils.writeLog(request, logUpdateBill);
+
+            } else
                 response.getWriter().write("false");
         } catch (Exception e) {
             response.getWriter().write("fail");
